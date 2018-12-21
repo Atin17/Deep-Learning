37a8965b4de8f6d724b395c6ad83fe5715b1fdb2
Atin Srivastava <atin.srivastava75@gmail.com>
2018-12-22 01:05:05 +0530

    Add files via upload

---
+++ ANN/ass2.py
@@ -0,0 +1,110 @@
+from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+
+pickle_file = 'notMNIST.pickle'
+
+with open(pickle_file, 'rb') as f:
+  save = pickle.load(f)
+  train_dataset = save['train_dataset']
+  train_labels = save['train_labels']
+  valid_dataset = save['valid_dataset']
+  valid_labels = save['valid_labels']
+  test_dataset = save['test_dataset']
+  test_labels = save['test_labels']
+  del save  # hint to help gc free up memory
+  print('Training set', train_dataset.shape, train_labels.shape)
+  print('Validation set', valid_dataset.shape, valid_labels.shape)
+  print('Test set', test_dataset.shape, test_labels.shape)
+
+image_size = 28
+num_labels = 10
+
+def reformat(dataset, labels):
+  dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+  # Map 1 to [0.0, 1.0, 0.0 ...], 2 to [0.0, 0.0, 1.0 ...]
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  return dataset, labels
+
+
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
+
+
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500 
+n_nodes_hl3 = 500
+
+n_classes = 10
+batch_size = 1024
+
+x=tf.placeholder('float',[None, 784])
+y=tf.placeholder('float')
+
+
+
+def neural_network_model(data):
+    
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1,n_nodes_hl2])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2,n_nodes_hl3])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3,n_classes])),
+                    'biases':tf.Variable(tf.random_normal([n_classes]))}
+
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
+
+    return output
+
+
+
+def train_neural_network(x):
+    
+    prediction = neural_network_model(x)
+    cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= prediction, labels= y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    hm_epochs = 10
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss=0
+            i = 0
+            while i < len(train_dataset):
+                start = i
+                end = i + batch_size
+
+                epoch_x = train_dataset[start:end]
+                epoch_y = train_labels[start:end]
+                
+                i += batch_size
+                _, c = sess.run([optimizer,cost], feed_dict = {x: epoch_x, y: epoch_y})
+                epoch_loss += c
+            print('Epoch', epoch+1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+            
+        correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:', accuracy.eval({x:test_dataset, y:test_labels}))
+
+train_neural_network(x)
+        
+        
